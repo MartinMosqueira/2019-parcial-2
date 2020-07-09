659d522021a2b310ebcbb2817df3daa6be922e0d
MartinMosqueira <yomartinmosqueira331@gmail.com>
2020-07-09 12:01:20 -0300

    parcial corregido

--- 56193-MartinMosqueira/printer/test_Printer.py
+++ 56193-MartinMosqueira/printer/test_Printer.py
@@ -2,15 +2,35 @@ import unittest
 from printer  import Printer
 
 class Test_Printer(unittest.TestCase):
-    def test_printer_available(self):
-        result=Printer().printer_available()
-        self.assertTrue(result)
 
-    def print_job(self):
-        result=Printer()
-        result.add_print_job('hola')
-        result1=result.error_flag
-        self.assertFalse(result1)
+    def test_printer_available(self):
+        printer=Printer()
+        self.assertTrue(printer.printer_available())
+        printer.add_print_job('tarea')
+        self.assertTrue(printer.print_job)
+    
+    def test_printer_not_available(self):
+        printer=Printer()
+        printer.add_print_job('tarea')
+        printer.print_job()
+        self.assertFalse(printer.printer_available())
+
+    def test_printer_nothing_print(self):
+        printer=Printer()
+        printer.print_job()
+        self.assertTrue(printer.error_flag)
+        self.assertEqual(printer.error_description,"nothing to print")
+    
+    def test_queue_printer(self):
+        printer=Printer()
+        self.assertTrue(printer.printer_available())
+        printer.add_print_job('tarea1')
+        printer.add_print_job('tarea2')
+        printer.print_job()
+        self.assertFalse(printer.printer_available())
+        printer.reset_printer()
+        self.assertTrue(printer.printer_available())
+
 
 if __name__ == '__main__':
    unittest.main()
--- 56193-MartinMosqueira/telefono/telefono.py
+++ 56193-MartinMosqueira/telefono/telefono.py
@@ -1,21 +1,38 @@
 
 class Telefono():
     def __init__(self):
-        self.cont=0
+        self.agregar=False
         self.credito=0
-
-    def dial(self,digitos):
-        if len(digitos) == 3 and (digitos[0] == '9' or digitos[0] == '8'):
-            return True
-
-        if len(digitos) >= 7:
-            if self.credito >= 10:
+    
+    
+    def dial(self,numero):
+        if len(numero)== 3:
+            if numero[0] == '9' or numero[0] == '8':
                 return True
+        
+        if len(numero) == 7:
+            if self.agregar == True:
+                if self.credito > 10:
+                    self.credito=self.credito-10
+                    return True
+                else:
+                    return False
             else:
                 return False
-        return False
-
-    def agregar_credito(self, digitos):
-        self.credito=self.credito-10
-        self.credito+=int(digitos)
-
+        
+        if numero[0] == '0' and numero[1] == '0':
+            if self.agregar == True:
+                if self.credito > 100:
+                    self.credito=self.credito-100
+                    return True
+                else:
+                    return False
+            else:
+                return False
+            
+    
+    def agregar_credito(self,credito):
+        
+        if credito > 10 or credito > 100:
+            self.agregar=True
+            self.credito=credito
